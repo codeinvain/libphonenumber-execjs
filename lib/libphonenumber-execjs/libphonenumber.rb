56be956b7aef3f49d517092614f757e00620f3c7
Daniel Cohen <daniel@codeinvain.com>
2012-03-22 07:30:18 +0200

    fixed parse method execution in scope

--- lib/libphonenumber-execjs/libphonenumber.rb
+++ lib/libphonenumber-execjs/libphonenumber.rb
@@ -1,34 +1,37 @@
 class Libphonenumber
-  
+
   attr_reader :context
-  
+
   def initialize
     @context = ExecJS.compile(File.read(File.join(File.dirname(__FILE__), "..", "..", "support", "libphonenumber.js")))
   end
-  
+
   def extractPossibleNumber(str="")
-    context.call "i18n.phonenumbers.PhoneNumberUtil.extractPossibleNumber", str
+    context.call "i18n.phonenumbers.PhoneNumberUtil.extractPossibleNumber",str
   end
-  
+
   def normalize(str="")
-    context.call "i18n.phonenumbers.PhoneNumberUtil.normalize", str
+    context.call "i18n.phonenumbers.PhoneNumberUtil.normalize",str
   end
-  
-  def parse(str="", default_region="ZZ")
-    context.call "i18n.phonenumbers.PhoneNumberUtil.getInstance().parse", str, default_region
+
+  def parse(str="", default_region="")
+    context.eval "i18n.phonenumbers.PhoneNumberUtil.getInstance().parse(#{make_param(str)},#{make_param(default_region)})"
   end
-  
+
   def simple
     @simple ||= Simple.new(self)
   end
-  
+
+  def make_param(str)
+    "'#{str.gsub(/[']/, '\\\\\'')}'"
+  end
   class Simple
-    
+
     def initialize(libphonenumber)
       @libphonenumber = libphonenumber
       @context = @libphonenumber.context
     end
-    
+
     def get_e164_phone_number(str, cc=nil, ndc=nil)
       @context.call "getE164PhoneNumber", str, cc, ndc
     end
@@ -36,7 +39,7 @@ class Libphonenumber
     def get_e164_phone_number_with_meta(str, cc=nil, ndc=nil)
       @context.call "getE164PhoneNumberWithMeta", str, cc, ndc
     end
-    
+
   end
-  
+
 end
